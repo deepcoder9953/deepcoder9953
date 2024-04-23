b6edf1812c6c0258cc415fafecc19c8f5fb03622
deepcoder9953 <165238037+deepcoder9953@users.noreply.github.com>
2024-04-23 14:27:16 +0530

    Add files via upload

---
+++ arrayassigment.js
@@ -0,0 +1,20 @@
+//problem:1
+var vegetable = ["tomato", "patato", "lady-finger"]
+console.log(vegetable);
+
+//problem:2
+var price = ["pencil cost: 23", "pen cost: 234", "eraser cost; 2345"]
+console.log(price[2]);
+
+// problem:3
+var superheroes = ["superman", "batman"]
+superheroes.push("spiderman", "Hulk", "Thor");
+console.log(superheroes);
+
+// problem:4
+var movies = ["DDLJ", "Tere naam", "Suryavansham"]
+var actor = ["Hrhitik", "Salman", "Amithabh"]
+console.log(movies[0],":-", actor[0] ,",", movies[1],":-", actor[1] ,",", movies[2],":-", actor[2]);
+
+
+
