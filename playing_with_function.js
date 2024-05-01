a4ca0d8ce956ea1b252861f8e1549955bb1f10c0
deepcoder9953 <165238037+deepcoder9953@users.noreply.github.com>
2024-05-01 15:07:40 +0530

    Add files via upload

---
+++ playing_with_function.js
@@ -0,0 +1,90 @@
+// var num = (12, 43, 54, 43, 32, 66)
+// var length = num.length;
+// function evenimplementation(num){
+//     for(var i = 0; i < length; i++){
+//     if(num[i] % 2 == 0){
+//         console.log("even");
+//     }
+// }
+// }
+// console.log(evenimplementation(12));  
+//.....................
+// var celcious = 0;
+// function temp(celcious){
+//     return (celcious*9/5)+32
+// }
+// console.log(temp(32),"fehrenheit");
+// console.log(temp(12),"fehrenheit");
+// console.log(temp(22),"fehrenheit");
+// console.log(temp(52),"fehrenheit");
+// console.log(temp(32),"fehrenheit");
+// console.log(temp(4232),"fehrenheit");
+// console.log(temp(53442),"fehrenheit");
+//................
+// var num1 = 0;
+// var num2 = 0;
+// var num3 = 0;
+// function large(num1, num2, num3){
+// if(num1 > num2 && num1 > num3){
+// }else if(num2 > num3 && num2 > num1){
+//     }else if(num3 > num1 && num3 > num2){
+// }
+// return large
+// }
+// console.log(large(21, 43, 55));
+//..................
+// function reversestring(text){
+//     let reversestring = " ";
+//     for(char of text){
+//         reversestring = char + reversestring;
+//     }
+//     return reversestring; 
+// }
+// console.log(reversestring("hello world"));
+// console.log(reversestring("allahmudillah"));
+// console.log(reversestring("menifest over procastinate"));
+//..................
+// var firstname = " ";
+// var lastname = " ";
+// var mergedname = " ";
+// function charname(firstname, lastname){
+//     var mergedname = firstname.concat(" "+lastname)
+//     console.log(mergedname);
+// }
+// return charname("namo", "Ghat")
+
+// function area(radious){
+//     return Math.PI*(radious*radious)
+// }
+// console.log(area(22));
+// console.log(area(2));
+// console.log(area(32));
+// console.log(area(52));
+// console.log(area(3322));
+
+//.....................................
+//two num calculator
+
+// let num1 = 0;
+// let num2 = 0;
+// operation = ('+','-','*','/')
+// total = 0
+// function calculator(operation){
+//     if(operation == '+'){
+//         return num1 + num2 == total
+//     }
+//     else if(operation == '-'){
+//         return num1 - num2 == total
+//     }
+//     else if(operation == '*'){
+//         return num1 * num2 == total
+//     }
+//     else if(operation == '/'){
+//         return num1 / num2 == total
+//     }
+//     return operation
+// }
+// console.log(calculator(234567+65432));
+// console.log(calculator(234567-65432));
+// console.log(calculator(234567*65432));
+// console.log(calculator(234567/65432));
